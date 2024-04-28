d5118be9dc6e806491219eb1b49b92a3c43f7023
donghun0508 <alswn4516@gmail.com>
2024-04-28 17:50:38 +0900

    first commit

---
+++ src/test/java/com/example/ecommercegateway/ECommerceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
